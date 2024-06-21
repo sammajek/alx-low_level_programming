c88a0df22486b5cf121b1f96a018208c274b27eb
sammajek <samtonmajek13@gmail.com>
2024-06-21 13:48:46 -0700

    Script

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints "Alphabet in Lowercase" to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		{
+		putchar(letter);
+		}
+	putchar('\n');
+
+	return (0);
+}
+
