e98017793d4d9608bdb7039c93d6d203ff060673
sammajek <samtonmajek13@gmail.com>
2024-06-21 03:56:17 -0700

    Script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
-int main(void)   /* To Print */ 
+/**
+ * Function Main
+ * Description - Long description
+ */
+int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
