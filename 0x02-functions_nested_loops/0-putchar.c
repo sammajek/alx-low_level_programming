3c22b0a18287265078256121c1b80e2f4641a59b
sammajek <samtonmajek13@gmail.com>
2024-06-25 08:03:22 -0700

    Print

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints "_Putchar" to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+My Functions Nested loops README
