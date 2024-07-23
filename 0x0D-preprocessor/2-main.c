4ee4b4170b6164dc194e372345ab2664d94b023b
sammajek <samtonmajek13@gmail.com>
2024-07-23 15:38:31 -0700

    Print

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,24 @@
-#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints the name of the file it was compiled
+ * from, followed by a new line.
+ *
+ * Return: Always returns 0 to indicate success.
+ */
 
 int main(void)
 {
 	const char *filename = __FILE__;
+
 	while (*filename)
 	{
 		write(1, filename, 1);
 		filename++;
 	}
 	write(1, "\n", 1);
+
 	return (0);
 }
