4fba91c7eca63699935a19580d6df4ed5814889a
sammajek <samtonmajek13@gmail.com>
2024-07-24 15:14:16 -0700

    Print

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Searches for an integer in an array
+ * @array: The array to search
+ * @size: The number of elements in the array
+ * @cmp: Pointer to the function to be used to compare values
+ *
+ * Return: The index of the first element for which cmp does not return 0,
+ * or -1 if no element matches or if size is less than or equal to 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp && size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+			{
+				return (i);
+			}
+		}
+	}
+
+	return (-1);
+}
