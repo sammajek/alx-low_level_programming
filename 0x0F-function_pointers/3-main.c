2bfab71ce25cbf203b6cc6227df4513004b2f7a8
sammajek <samtonmajek13@gmail.com>
2024-07-25 10:38:24 -0700

    Print

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 4)
 	{
-	write(2, "Error\n", 6);
+	write(2, "Error\n98\n", 12);
 		exit(98);
 	}
 
@@ -25,10 +25,15 @@ int main(int argc, char *argv[])
 	num2 = atoi(argv[3]);
 
 	op_func = get_op_func(argv[2]);
-	if (!op_func)
+	if (op_func == NULL || argv[2][1] != '\0')
 	{
-		write(2, "Error\n", 6);
+		write(2, "Error\n99\n", 12);
 		exit(99);
+	}
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	{
+		write(2, "Error\n100\n", 12);
+		exit(100);
 	}
 
 	result = op_func(num1, num2);
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -49,7 +49,7 @@ int op_div(int a, int b)
 {
 	if (b == 0)
 	{
-		write(2, "Error\n", 6);
+		write(2, "Error\n100\n", 12);
 		exit(100);
 	}
 	return (a / b);
@@ -66,7 +66,7 @@ int op_mod(int a, int b)
 {
 	if (b == 0)
 	{
-		write(2, "Error\n", 6);
+		write(2, "Error\n100\n", 12);
 		exit(100);
 	}
 	return (a % b);
