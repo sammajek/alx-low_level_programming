da23b7d25e846ee28974561e95a03132870efc04
sammajek <samtonmajek13@gmail.com>
2024-07-31 08:45:10 -0700

    Print

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,21 +7,15 @@
 void more_numbers(void)
 {
 	int i, j;
-	char numbers[150];
-	char *ptr = numbers;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 15; j++)
+		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
-				*ptr++ = '1';
-			*ptr++ = '0' + (j % 10);
+				_putchar('1');
+			_putchar((j % 10) + '0');
 		}
-		*ptr++ = '\n';
+		_putchar('\n');
 	}
-	*ptr = '\0';
-
-	for (ptr = numbers; *ptr; ptr++)
-		_putchar(*ptr);
 }
