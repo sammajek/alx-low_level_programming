b2ff454e94e92eb265060a976702060bb18a4a8e
sammajek <samtonmajek13@gmail.com>
2024-07-31 09:53:56 -0700

    Print

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * find_largest_prime_factor - Finds and prints the largest prime factor of a number
+ * find_largest_prime_factor - Finds the largest prime factor of a number
  * @n: The number to find the largest prime factor of
  */
 void find_largest_prime_factor(long n)
@@ -22,7 +22,7 @@ void find_largest_prime_factor(long n)
 			factor++;
 		}
 	}
-    
+
 	printf("%ld\n", largest);
 }
 
@@ -33,5 +33,5 @@ void find_largest_prime_factor(long n)
 int main(void)
 {
 	find_largest_prime_factor(612852475143);
-	return 0;
+	return (0);
 }
